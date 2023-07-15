750c5cdb72e43fe991e2141732bf999534bf91c1
ashish__12 <ashish.suresh@finalyca.com>
2023-07-15 15:44:58 +0530

    edited paragraph

--- header.js
+++ header.js
@@ -1,3 +1,9 @@
+const sideBar = document.querySelector(".sidebar");
+const sideBarClose = document.querySelector(".btnd");
+const closeBtn = document.querySelector('.closebtn')
+sideBarClose.addEventListener('click',() =>  sideBar.classList.add('openNav'));
+closeBtn.addEventListener('click', () => sideBar.classList.remove('openNav'));
+
 
 
 
